1e7620fe82224e163dcdbd0b31d892bf2fe2b53a
EL GATO <142356190+Scriptbros@users.noreply.github.com>
2023-09-15 18:34:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Simulate a 10-second loading process
+    // Simulate a 3-second loading process
     setTimeout(function() {
         var loading = document.getElementById('loading');
         var content = document.getElementById('content');
 
         loading.style.display = 'none';
         content.style.display = 'block';
-    }, 10000);
-});
+    }, 3000);
 
+    // Function to toggle project details visibility
+    function toggleProjectDetails(projectId) {
+        var details = document.getElementById(`projectDetails${projectId}`);
+        details.style.display = details.style.display === 'none' ? 'block' : 'none';
+    }
+
+    // Assign event listeners to project list items
+    var projectItems = document.querySelectorAll('.project-item');
+    projectItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            var projectId = this.dataset.projectId;
+            toggleProjectDetails(projectId);
+        });
+    });
+});

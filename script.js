48b4ec2d1699c355d26327ccef2e9d5dc6878814
EL GATO <142356190+Scriptbros@users.noreply.github.com>
2023-09-15 18:19:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Simulate a 10-second loading process
+    setTimeout(function() {
+        var loading = document.getElementById('loading');
+        var content = document.getElementById('content');
+
+        loading.style.display = 'none';
+        content.style.display = 'block';
+    }, 10000);
+});
 

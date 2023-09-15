ed4f31e96464aa017bcae716a8a382c63d63b850
EL GATO <142356190+Scriptbros@users.noreply.github.com>
2023-09-15 19:28:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var parallaxElement = document.querySelector('.parallax');
     var parallaxInstance = new Parallax(parallaxElement);
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var facts = document.querySelectorAll('.fact');
+    var currentFactIndex = 0;
+
+    function showNextFact() {
+        facts[currentFactIndex].classList.remove('active');
+        currentFactIndex = (currentFactIndex + 1) % facts.length;
+        facts[currentFactIndex].classList.add('active');
+    }
+
+    setInterval(showNextFact, 5000); // Show a new fact every 5 seconds
+});
